a390b2e86c31b5aca228bdd21e4053536781db53
Adrien GRAMPONE <atale1608@hotmail.fr>
2021-12-07 15:15:11 +0100

    initial commit

---
+++ services/backend.js
@@ -0,0 +1,17 @@
+const users = [
+    {
+        id: "1",
+        username : "toto"
+    },
+    {
+        id: "2",
+        username: "tata"
+    }
+]
+export default {
+    getUsers(){
+        return new Promise((resolve) => {
+            resolve(users)
+        })
+    }
+}
